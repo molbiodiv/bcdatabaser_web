b0e675215aee1e89168a1385c21169cf8496b076
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-12-09 08:36:07 +0100

    Update libs

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/scripts', express.static(__dirname + '/node_modules/'));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
