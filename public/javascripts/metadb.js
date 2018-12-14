1820ac94ece2f17cd2bed8a355be5170ea3357dd
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-12-14 16:25:50 +0100

    Update parameters

--- public/javascripts/metadb.js
+++ public/javascripts/metadb.js
@@ -23,6 +23,7 @@ $(function() {
       params.push(sequenceLengthFilter)
     }
     params = ['metabDB/bin/reference_db_creator.pl', '--marker-search-string', 'ITS2', '--taxonomic-range', 'Bellis', '--sequence-length-filter', '100:2000']
+    //params = ['metabDB/bin/reference_db_creator.pl']
     console.log(params)
     socket.emit('execute', {
       parameters: params
